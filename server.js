4280734b660eb45861b924f0693254b322ded59b
Opilane TTHK <opilane@tthk.ee>
2020-09-30 10:15:31 +0300

    commit changes

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const ejs = require('ejs');
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', ejs);
+app.get('/date', (req, res)=> {
+    let today = new Date();
+    let day = '';
+    let personData = {firstName: "John",
+                lastName: "Doe",
+                age: 34,
+                job: "Developer"
+                };
+
+    if(today.getDay() === 6 || today.getDate() === 0){
+        day = 'Weekend!';
+    }else {
+        day = 'Workday';
+    }
+    res.render('index.ejs', {kindofDay: day, 
+                            person: personData
+                            });
+});
+
+app.listen(3000, ()=> {
+    console.log('Server is running on port 3000.');
+});
